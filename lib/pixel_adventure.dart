d821c75aa170c14843ccf8eabd4147a471eb7915
Manuel Werder <manuelwerder@hotmail.com>
2025-01-18 12:54:44 +0100

    Added a fist tilemap and level

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,3 +1,22 @@
-import 'package:flame/game.dart';
+import 'dart:async';
 
-class PixelAdventure extends FlameGame {}
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:pixel_adventure/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  late final CameraComponent cam;
+  final level = Level();
+
+  @override
+  FutureOr<void> onLoad() async {
+    cam = CameraComponent.withFixedResolution(
+      world: level,
+      width: 640,
+      height: 360,
+    );
+    cam.viewfinder.anchor = Anchor.topLeft;
+    addAll([cam, level]);
+    return super.onLoad();
+  }
+}
