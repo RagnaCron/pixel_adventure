76efc7fc88eb9b6667e6662075dca6f0189bedd7
Manuel Werder <manuelwerder@hotmail.com>
2025-01-18 16:57:25 +0100

    Loading the player on a spawnpoint

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -3,15 +3,22 @@ import 'package:pixel_adventure/pixel_adventure.dart';
 
 enum PlayerState {
   idle,
+  running,
+  jumping,
+  falling,
+  doubleJumping,
+  wallJumping,
 }
 
 class Player extends SpriteAnimationGroupComponent
     with HasGameReference<PixelAdventure> {
-  late final SpriteAnimation idleAnimation;
+  String character;
 
-  final int amount = 11;
+  Player({super.position, required this.character});
+
+  late final SpriteAnimation idleAnimation;
+  late final SpriteAnimation runningAnimation;
   final double stepTime = 0.05;
-  final Vector2 textureSize = Vector2.all(32);
 
   @override
   Future<void> onLoad() async {
@@ -21,12 +28,26 @@ class Player extends SpriteAnimationGroupComponent
   }
 
   void _loadAllAnimations() {
-    idleAnimation = SpriteAnimation.fromFrameData(
-      game.images.fromCache('Mask Dude/Idle (32x32).png'),
+    idleAnimation = _spriteAnimation('Idle', 11);
+    runningAnimation = _spriteAnimation('Run', 12);
+
+    // List of all animations
+    animations = {
+      PlayerState.idle: idleAnimation,
+      PlayerState.running: runningAnimation,
+    };
+
+    // Set current animation
+    current = PlayerState.idle;
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount) {
+    return SpriteAnimation.fromFrameData(
+      game.images.fromCache("Main Characters/$character/$state (32x32).png"),
       SpriteAnimationData.sequenced(
         amount: amount,
         stepTime: stepTime,
-        textureSize: textureSize,
+        textureSize: Vector2.all(32),
       ),
     );
   }
